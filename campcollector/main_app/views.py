32b1db9bc7a39c135c187f183dbea4f48bc84d4a
CToy17 <csharrow1994@gmail.com>
2022-05-17 18:00:39 -0700

    add day3

--- campcollector/main_app/views.py
+++ campcollector/main_app/views.py
@@ -1,5 +1,7 @@
+from dataclasses import field
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Camp
 
 import logging
@@ -42,3 +44,17 @@ def campground_detail(request, camp_id):
     logging.info('calling camp_info')
     camp = Camp.objects.get(id=camp_id)
     return render(request, 'campgrounds/details.html', {'camp':camp})
+
+class CampCreate(CreateView):
+    model = Camp
+    fields = '__all__'
+    success_url = '/campgrounds/'
+
+class CampUpdate(UpdateView):
+    model = Camp
+    fields = ['activities']
+    success_url = '/campgrounds/'
+
+class CampDelete(DeleteView):
+  model = Camp
+  success_url = '/campgrounds/'
